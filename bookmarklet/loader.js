c25145fd10d2bd5e64e888afacb3382813422a6a
miyoshi <contact@colspan.net>
2008-10-22 21:22:07 +0900

    made bookmark execution stable

--- bookmarklet/loader.js
+++ bookmarklet/loader.js
@@ -20,6 +20,9 @@ if( typeof( JS_IM_Core_Loaded ) == 'undefined' ){
 	function JS_IM_B_Load( methodName ){
 		var i,l;
 		var srcPrefix = 'scripts/';
+
+		if( methodName == null ) return;
+
 		if( typeof( JS_IM_Core_Loaded ) == 'undefined' ){
 			for( i = 0, l = JS_IM_Src.core.length; i < l; i++ ){
 				JS_IM_B_JSLoader( srcPrefix + JS_IM_Src.core[i] );
@@ -28,10 +31,15 @@ if( typeof( JS_IM_Core_Loaded ) == 'undefined' ){
 			JS_IM_Core_Loaded = true;
 		}
 		if( JS_IM_Src[methodName] && ! JS_IM_Loaded[methodName] ){
+			if( JS_IM_CurrentMethod != null ) JS_IM_B_Disable();
 			for( i = 0, l = JS_IM_Src[methodName].length; i < l; i++ ){
 				JS_IM_B_JSLoader( srcPrefix + JS_IM_Src[methodName][i] );
 			}
 			JS_IM_Loaded[methodName] = true;
+		}
+
+		if( JS_IM_CurrentMethod != methodName ){
+			JS_IM_CurrentMethod = methodName;
 			setTimeout( 'JS_IM_B_Execute( "' + methodName + '" )', 1000 );
 		}
 		else{
@@ -54,13 +62,20 @@ if( typeof( JS_IM_Core_Loaded ) == 'undefined' ){
 		for( i=0;i<textareas.length;i++ ) textareas[i].JS_IM_Obj.toggle();
 		for( i=0;i<inputs.length;i++) if( inputs[i].type == "text" ) inputs[i].JS_IM_Obj.toggle();
 	}
+	function JS_IM_B_Disable(){
+		var textareas = document.getElementsByTagName('textarea');
+		var inputs = document.getElementsByTagName('input');
+		var i;
+		for( i=0;i<textareas.length;i++ ) textareas[i].JS_IM_Obj.disable();
+		for( i=0;i<inputs.length;i++) if( inputs[i].type == "text" ) inputs[i].JS_IM_Obj.disable();
+	}
 
 
 
 
 	//JS_IM_URL_Prefix = 'http://colspan.net/hangulime/';
-	JS_IM_URL_Prefix = './';
-	//JS_IM_URL_Prefix = 'http://colspan.net/experiment/jsim/';
+	//JS_IM_URL_Prefix = './';
+	JS_IM_URL_Prefix = 'http://colspan.net/experiment/jsim/';
 	
 	JS_IM_Src = {};
 	JS_IM_Src.core = new Array(
@@ -81,6 +96,7 @@ if( typeof( JS_IM_Core_Loaded ) == 'undefined' ){
 	);
 
 	JS_IM_Loaded = {};
+	JS_IM_CurrentMethod = null;
 
 
 }
